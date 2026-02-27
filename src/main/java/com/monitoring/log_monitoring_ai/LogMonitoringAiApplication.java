ba9b3d65df88206a80d670a29d38ce9100799907
sahilajaz <sahilajazw@gmail.com>
2026-02-27 12:06:36 +0530

    first commit

---
+++ src/main/java/com/monitoring/log_monitoring_ai/LogMonitoringAiApplication.java
@@ -0,0 +1,13 @@
+package com.monitoring.log_monitoring_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogMonitoringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogMonitoringAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monitoring/log_monitoring_ai/LogMonitoringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitoring.log_monitoring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogMonitoringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
